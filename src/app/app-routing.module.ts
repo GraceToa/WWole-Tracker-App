ec74de44fc578db121722174016b179bcb40382a
GraceToa <gracetoa29@gmail.com>
2020-10-21 15:34:25 +0200

    change path tabls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./pages/search/search.module').then( m => m.SearchPageModule)
+  },
+  {
+    path: 'detail-employee',
+    loadChildren: () => import('./pages/detail-employee/detail-employee.module').then( m => m.DetailEmployeePageModule)
   }
 ];
 @NgModule({
--- src/providers/employees.service.ts
+++ src/providers/employees.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import {map, catchError} from 'rxjs/operators';
 
+//providers
 import  {URL_SERVICES} from "../config/url.services";
 
 @Injectable({
@@ -11,24 +12,38 @@ import  {URL_SERVICES} from "../config/url.services";
 export class EmployeesService {
 
   employees:any[] = [];
+  item:any={};
 
   constructor(protected httpClient: HttpClient) {
     this.get_all_employees();
   }
 
- get_all_employees(){
-   let promise = new Promise((resolve)=>{
-     this.httpClient.get(URL_SERVICES).pipe(map(res=>res))
-         .subscribe((data:any)=>{
-           console.log(data);
-           if(data){
-             this.employees.push(...data);
-           }else{
-             
-           }
-           resolve();
-         })
-     });
-     return promise;
- }
+   get_all_employees(){
+     let promise = new Promise((resolve)=>{
+       this.httpClient.get(URL_SERVICES).pipe(map(res=>res),catchError(this.handleError))
+           .subscribe((data:any)=>{
+             console.log(data);
+             if(data){
+               this.employees.push(...data);
+             }else{
+               console.log("unsucessfully data")
+             }
+             resolve();
+           })
+       });
+       return promise;
+   }
+
+   private handleError (error: Response | any) {
+      let errMsg: string;
+      if (error instanceof Response) {
+        const err = error || '';
+        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+      } else {
+        errMsg = error.message ? error.message : error.toString();
+      }
+      console.error(errMsg);
+      return errMsg;
+    }
+
 }
